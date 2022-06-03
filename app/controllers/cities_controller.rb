3703e86b5cee76d8fe067bba03327119314846ad
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-06-03 00:47:19 -0300

    Saving key with rails credentials

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,22 +1,18 @@
 class CitiesController < ApplicationController
 
     def new
-
     end
 
     def index
-        @response
     end
 
     def get_locations
-        cidade = params[:name]
-        url = "https://api.weatherapi.com/v1/history.json?key=c843ecec3a444ce7bf0143506223005&q=#{cidade}&dt=#{1.week.ago.strftime("%Y-%m-%d")}"
+        city = params[:name]
+        key = Rails.application.credentials.api[:key]
+        date = params[:date]
+        url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
         @response = RestClient.get(url)
-        p @response.body
-        # respond_to do |format|
-        #     format.html { render json: response.body.to_json }
-        # end
-        # render json: @@response.body, status: :ok
-        redirect_to controller: "cities", action: "index", dados: @response
+        # p @response.body
+        render json: @response.body, status: :ok
     end
 end
