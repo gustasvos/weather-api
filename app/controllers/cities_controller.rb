ad5b3ee3f0cc2f3e7c15bc7765853425659effe8
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-06-07 20:40:38 -0300

    Show weather history for a day

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -11,7 +11,8 @@ class CitiesController < ApplicationController
         key = Rails.application.credentials.api[:key]
         url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
         response = RestClient.get(url)
-        @a = JSON.parse(response.body).symbolize_keys
+        @response = JSON.parse(response.body).symbolize_keys
+        # byebug
         render :index, status: :unprocessable_entity
     end
 end
