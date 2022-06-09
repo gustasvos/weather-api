8ce9c247420ca70b7d73971d9fd7c30588d03003
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-06-09 14:53:13 -0300

    Reorganize views

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -9,10 +9,14 @@ class CitiesController < ApplicationController
         city = params[:name]
         date = params[:date]
         key = Rails.application.credentials.api[:key]
-        url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
-        response = RestClient.get(url)
-        @response = JSON.parse(response.body).symbolize_keys
-        # byebug
+        history_url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
+        responseh = RestClient.get(history_url)
+        @responseh = JSON.parse(responseh.body).symbolize_keys
+
+        current_url = "http://api.weatherapi.com/v1/current.json?key=#{key}&q=#{city}&aqi=no"
+        responsec = RestClient.get(current_url)
+        @responsec = JSON.parse(responsec.body).symbolize_keys
+
         render :index, status: :unprocessable_entity
     end
 end
