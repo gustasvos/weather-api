1e5f5dbddaa8ce9f5a9bc310f8356e4faa403978
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-06-21 14:10:31 -0300

    error route for when params is missing

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.7.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.3"
+gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
@@ -28,7 +28,7 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
-gem "redis", "~> 4.0"
+# gem "redis", "~> 4.0"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -52,6 +52,8 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'byebug'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -65,10 +67,4 @@ group :development do
   # gem "spring"
 end
 
-group :test do
-  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem "capybara"
-  gem "selenium-webdriver"
-  gem "webdrivers"
-end
 gem "rest-client", "~> 2.1"
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,20 +3,27 @@ class CitiesController < ApplicationController
     end
 
     def index
+        get_locations()
     end
-
+    
+    private
     def get_locations
         city = params[:name]
         date = params[:date]
         key = Rails.application.credentials.api[:key]
-        history_url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
-        responseh = RestClient.get(history_url)
-        @responseh = JSON.parse(responseh.body).symbolize_keys
 
-        current_url = "http://api.weatherapi.com/v1/current.json?key=#{key}&q=#{city}&aqi=no"
-        responsec = RestClient.get(current_url)
-        @responsec = JSON.parse(responsec.body).symbolize_keys
+        if city.blank? || date.blank?
+            render :error, status: :bad_request
+        else 
+            history_url = "https://api.weatherapi.com/v1/history.json?key=#{key}&q=#{city}&dt=#{date}"
+            responseh = RestClient.get(history_url)
+            @responseh = JSON.parse(responseh.body).symbolize_keys
+    
+            current_url = "http://api.weatherapi.com/v1/current.json?key=#{key}&q=#{city}&aqi=no"
+            responsec = RestClient.get(current_url)
+            @responsec = JSON.parse(responsec.body).symbolize_keys
+            render :index
+        end
 
-        render :index, status: :unprocessable_entity
     end
 end
